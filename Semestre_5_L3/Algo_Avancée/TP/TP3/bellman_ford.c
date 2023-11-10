c461576ba83f189554208299711332706f125669
Viet281101 <nva281101@gmail.com>
2023-11-10 14:42:01 +0100

    update

--- Semestre_5_L3/Algo_Avancée/TP/TP3/bellman_ford.c
+++ Semestre_5_L3/Algo_Avancée/TP/TP3/bellman_ford.c
@@ -67,10 +67,16 @@ int main() {
 
     // Exemple: 
     graph->edge[0].source = 0, graph->edge[0].destination = 1, graph->edge[0].weight = -1;
+    graph->edge[1].source = 0, graph->edge[1].destination = 2, graph->edge[1].weight = 4;
+    graph->edge[2].source = 1, graph->edge[2].destination = 2, graph->edge[2].weight = 3;
+    graph->edge[3].source = 1, graph->edge[3].destination = 3, graph->edge[3].weight = 2;
+    graph->edge[4].source = 1, graph->edge[4].destination = 4, graph->edge[4].weight = 2;
 
     clock_t t0, t1, dt;
     t0 = clock();
+
     bellman_ford(graph, 0);
+    
     t1 = clock();
     dt = t1 - t0;
     printf("Temps d'exécution : %f secondes\n", (double) dt / CLOCKS_PER_SEC);
