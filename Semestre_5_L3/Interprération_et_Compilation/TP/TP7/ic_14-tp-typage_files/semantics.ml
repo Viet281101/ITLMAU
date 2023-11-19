366774a50baa591c13e3b2370e6132f6891d13bb
Viet281101 <nva281101@gmail.com>
2023-11-19 06:22:56 +0100

    update

--- Semestre_5_L3/Interprération_et_Compilation/TP/TP7/ic_14-tp-typage_files/semantics.ml
+++ Semestre_5_L3/Interprération_et_Compilation/TP/TP7/ic_14-tp-typage_files/semantics.ml
@@ -22,33 +22,58 @@ let errt expected given pos =
 
 let env = ref Env.empty
 
+let check_type expected actual pos =
+  if expected <> actual then
+    errt expected actual pos
+
 let analyze_value value =
   match value with
-  | Syntax.Bool b -> Bool b
-  | Syntax.Int n  -> Int n
+  | Syntax.Bool b -> Bool_t, Bool b
+  | Syntax.Int n  -> Int_t, Int n
 
 let rec analyze_expr expr =
   match expr with
   | Syntax.Value v ->
-     Value (analyze_value v.value)
+     let typ, val' = analyze_value v.value in
+     typ, Value val'
   | Syntax.Var v ->
-     Var v.name
+      if Env.mem v.name !env then
+        let typ = Env.find v.name !env in
+        typ, Var v.name
+      else
+        raise (Error ("Unbound variable: " ^ v.name, v.pos))
   | Syntax.Call c ->
-     let args = List.map analyze_expr c.args in
-     Call (c.func, args)
-
+    if Env.mem c.func !env then
+      let arg_types, args = List.fold_right (fun arg (typs, aexprs) ->
+        let atyp, aexpr = analyze_expr arg in
+        (atyp :: typs, aexpr :: aexprs)) c.args ([], [])
+      in
+      let func_typ = Env.find c.func !env in
+      (match func_typ with
+       | Func_t (ret_typ, param_types) ->
+          if List.length param_types <> List.length arg_types then
+            raise (Error (Printf.sprintf "Incorrect number of arguments for %s" c.func, c.pos))
+          else
+            List.iter2 (fun expected actual ->
+              check_type expected actual c.pos) param_types arg_types;
+          ret_typ, Call (c.func, args)
+       | _ -> raise (Error ("Function type expected for " ^ c.func, c.pos)))
+    else
+      raise (Error ("Unbound function: " ^ c.func, c.pos))
 
 let analyze_instr instr =
   match instr with
   | Syntax.Decl d ->
-     env := Env.add d.name () !env;
+     env := Env.add d.name d.type_t !env;
      Decl d.name
   | Syntax.Assign a ->
-     let ae = analyze_expr a.expr in
      if Env.mem a.var !env then
+       let expected_typ = Env.find a.var !env in
+       let actual_typ, ae = analyze_expr a.expr in
+       check_type expected_typ actual_typ (expr_pos a.expr);
        Assign (a.var, ae)
      else
-       raise (Error ("unbound variable: " ^ a.var, expr_pos a.expr))
+       raise (Error ("Unbound variable: " ^ a.var, expr_pos a.expr))
 
 let rec analyze_block block =
   match block with
